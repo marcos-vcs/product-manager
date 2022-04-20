c34a851465bcf6dea6d436e849108c4a1be547eb
Marcos Sousa <marcosviniciocardoso123@gmail.com>
2022-04-20 13:36:19 -0300

    back-end initial commit

---
+++ src/main/java/br/com/product/mannager/ProductMannagerApplication.java
@@ -0,0 +1,13 @@
+package br.com.product.mannager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductMannagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductMannagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/product/mannager/ProductMannagerApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.product.mannager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductMannagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
