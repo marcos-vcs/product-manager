953b66ebf8d23eba1d0caf66864711cb4d5c90c8
Marcos Sousa <marcosviniciocardoso123@gmail.com>
2022-04-21 17:53:09 -0300

    feature: add components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,22 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { LoginComponent } from './security/login/login.component';
+import { RecoveryComponent } from './security/recovery/recovery.component';
+import { CreateComponent } from './security/create/create.component';
+import { ProductListComponent } from './application/product-list/product-list.component';
+import { ProductModalComponent } from './application/product-modal/product-modal.component';
+import { ConfirmDialogComponent } from './application/confirm-dialog/confirm-dialog.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    RecoveryComponent,
+    CreateComponent,
+    ProductListComponent,
+    ProductModalComponent,
+    ConfirmDialogComponent
   ],
   imports: [
     BrowserModule
