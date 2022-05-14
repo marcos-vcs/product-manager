3306b1d3b317a797865485d8f475b65126b3f871
Marcos Sousa <marcosviniciocardoso123@gmail.com>
2022-05-14 16:58:52 -0300

    update database response model

--- src/app/application/database.service.ts
+++ src/app/application/database.service.ts
@@ -3,7 +3,7 @@ import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { environment } from 'src/environments/environment';
 import { Product } from '../model/product';
-import { Response, ResponseCreate, ResponseString, ResponseUser } from '../model/response';
+import { Response } from '../model/response';
 import { User } from '../model/user';
 
 @Injectable({
@@ -13,37 +13,37 @@ export class DatabaseService {
 
   constructor(private http: HttpClient) { }
 
-  create(product: Product): Observable<ResponseCreate>{
-    return this.http.post<ResponseCreate>(`${environment.api}`, product, {headers: this.getHeader()});
+  create(product: Product): Observable<Response<Product>>{
+    return this.http.post<Response<Product>>(`${environment.api}`, product, {headers: this.getHeader()});
   }
 
-  update(product: Product): Observable<ResponseCreate>{
-    return this.http.put<ResponseCreate>(`${environment.api}`, product, {headers: this.getHeader()});
+  update(product: Product): Observable<Response<Product>>{
+    return this.http.put<Response<Product>>(`${environment.api}`, product, {headers: this.getHeader()});
   }
 
-  delete(code: string): Observable<ResponseCreate>{
-    return this.http.delete<ResponseCreate>(`${environment.api}?code=${code}`, {headers: this.getHeader()});
+  delete(code: string): Observable<Response<string>>{
+    return this.http.delete<Response<string>>(`${environment.api}?code=${code}`, {headers: this.getHeader()});
   }
 
-  get(skip: number, limit: Number): Observable<Response>{
-    return this.http.get<Response>(`${environment.api}?skip=${skip}&limit=${limit}`, {headers: this.getHeader()});
+  get(skip: number, limit: Number): Observable<Response<Product[]>>{
+    return this.http.get<Response<Product[]>>(`${environment.api}?skip=${skip}&limit=${limit}`, {headers: this.getHeader()});
   }
 
-  search(skip: number, limit: number, filter: string, search: string): Observable<Response>{
-    return this.http.get<Response>(
+  search(skip: number, limit: number, filter: string, search: string): Observable<Response<Product[]>>{
+    return this.http.get<Response<Product[]>>(
       `${environment.api}/search?skip=${skip}&limit=${limit}&filter=${filter}&search=${search}`, {headers: this.getHeader()});
   }
 
-  verifyUser(): Observable<ResponseUser>{
-    return this.http.get<ResponseUser>(`${environment.apiUser}`, {headers: this.getHeader()});
+  verifyUser(): Observable<Response<User>>{
+    return this.http.get<Response<User>>(`${environment.apiUser}`, {headers: this.getHeader()});
   }
 
-  createUser(user: User):  Observable<ResponseUser>{
-    return this.http.post<ResponseUser>(`${environment.apiUser}`, user, {headers: this.getHeader()});
+  createUser(user: User):  Observable<Response<User>>{
+    return this.http.post<Response<User>>(`${environment.apiUser}`, user, {headers: this.getHeader()});
   }
 
-  tokenToUid(): Observable<ResponseString>{
-    return this.http.get<ResponseString>(`${environment.apiUser}/token-to-uid`, {headers: this.getHeader()});
+  tokenToUid(): Observable<Response<string>>{
+    return this.http.get<Response<string>>(`${environment.apiUser}/token-to-uid`, {headers: this.getHeader()});
   }
 
   private getHeader(): HttpHeaders{
