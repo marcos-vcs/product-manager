b51fc4fd60b03ca18b711190ef37017c3b3ab59e
Marcos Sousa <marcosviniciocardoso123@gmail.com>
2022-05-22 11:57:38 -0300

    improvement: update home and routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,9 @@
 import { Routes } from "@angular/router";
 import { HomeComponent } from "./application/home/home.component";
 import { ProductListComponent } from "./application/product-list/product-list.component";
+import { ProductTrashComponent } from "./application/product-trash/product-trash.component";
+import { SupplierListComponent } from "./application/supplier-list/supplier-list.component";
+import { SupplierTrashComponent } from "./application/supplier-trash/supplier-trash.component";
 import { AuthenticationComponent } from "./security/authentication/authentication.component";
 import { CreateComponent } from "./security/create/create.component";
 import { LoginComponent } from "./security/login/login.component";
@@ -12,7 +15,11 @@ export const rootRouterConfig: Routes = [
         path: '',
         component: HomeComponent,
         children: [
-            {path: '', component: ProductListComponent}
+            {path: '', redirectTo: 'product-manager', pathMatch: 'full'},
+            {path: 'product-manager', component: ProductListComponent},
+            {path: 'product-trash', component: ProductTrashComponent},
+            {path: 'supplier-manager', component: SupplierListComponent},
+            {path: 'supplier-trash', component: SupplierTrashComponent}
         ],
         canActivate: [SecurityGuard]
     },
